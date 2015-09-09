cf04b3a5511ead938f2d9fe3f8a846b9ea9a1f8c
hikalium <hikarupsp@users.sourceforge.jp>
2015-09-09 18:32:44 +0900

    ひとまずAIが完成した。まあまあ強い。

--- gomoku.c
+++ gomoku.c
@@ -1,11 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #include "gomoku.h"
+
+char *stateString[] = {
+	".",
+	"●",
+	"○"
+};
+
+char *colorString[] = {
+	"None",
+	"Black",
+	"White"
+};
+
+void (*AIList[AI_LIST_LENGTH])(StoneLocation *to, GameEnvironment *env) = {
+	RandAI_decideNextLocation,
+	EasyAI_decideNextLocation
+};
+char *playerTypeString[AI_LIST_LENGTH + 1] = {
+	"Human",
+	"RandAI",
+	"EasyAI"
+};
 
 int main(int argc, char *argv[])
 {
-	int currentColor;
-	int currentPlayerType;
-	int i;
 	GameEnvironment env;
 	StoneLocation to;
 
@@ -13,50 +34,49 @@ int main(int argc, char *argv[])
 	printBoard(env.mainBoard);
 	selectGameMode(&env);
 
-	for(env.turnCount = 0; ; env.turnCount++){
-		currentColor = (env.turnCount & 1) + 1;
-		currentPlayerType = (currentColor == STATE_BLACK ? env.playerBlackType : env.playerWhiteType);
-		printf("\n\nTurn%3d, %s, %s\n", env.turnCount, playerTypeString[currentPlayerType], colorString[currentColor]);
+	srand(time(NULL));
 
-		// Think or Input.
-		if(currentPlayerType == PLAYER_TYPE_COM_RANDOM){
-			
-		} else if(currentPlayerType == PLAYER_TYPE_HUMAN){
-			printf("%s's turn.\n", colorString[currentColor]);
-			inputStoneLocation(&to);	
-		} else{
-			puts("Invalid plyer type. Abort.");
-			return 1;
-		}
-		printf("%s->(%d, %d)\n", colorString[currentColor], to.x, to.y);
-		
-		// Check and put stone.
-		if(putStone(env.mainBoard, to.x, to.y, currentColor)){
-			puts("################################################################");
-			if(currentPlayerType == PLAYER_TYPE_HUMAN){
-				puts("You can't put stone there. Try other location.");
-				env.turnCount--;
+	for(env.gameCount = 0; env.gameCount < env.gameCountLimit; env.gameCount++){
+		for(env.turnCount = 0; ; env.turnCount++){
+			env.currentColor = (env.turnCount & 1) + 1;
+			env.currentPlayerType = (env.currentColor == STATE_BLACK ? env.playerBlackType : env.playerWhiteType);
+			printf("\n\n%3d-%3d, %s, %s\n", env.gameCount, env.turnCount, playerTypeString[env.currentPlayerType], colorString[env.currentColor]);
+	
+			// Think or Input.
+			if(env.currentPlayerType == PLAYER_TYPE_HUMAN){
+				printf("%s's turn.\n", colorString[env.currentColor]);
+				inputStoneLocation(&to);	
+			} else if(env.currentPlayerType <= AI_LIST_LENGTH){
+				AIList[env.currentPlayerType - 1](&to, &env);
 			} else{
-				puts("COM tried to put stone to illegal location. Abort.");
+				puts("Invalid player type. Abort.");
 				return 1;
 			}
-			puts("################################################################");
-			continue;
-		}
-		printBoard(env.mainBoard);
-
-		// Goal test.
-		checkMapRows(env.mainBoard, env.rowList, ROW_LIST_LENGTH);
-		for(i = 0; i < ROW_LIST_LENGTH; i++){
-			if(env.rowList[i].length == 5){
+			printf("%s->(%d, %d)\n", colorString[env.currentColor], to.x, to.y);
+			
+			// Check and put stone.
+			if(putStone(env.mainBoard, to.x, to.y, env.currentColor)){
+				puts("################################################################");
+				if(env.currentPlayerType == PLAYER_TYPE_HUMAN){
+					puts("You can't put stone there. Try other location.");
+					env.turnCount--;
+				} else{
+					puts("COM tried to put stone to illegal location. Abort.");
+					return 1;
+				}
+				puts("################################################################");
+				continue;
+			}
+			printBoard(env.mainBoard);
+			if(isGameEnd(&env)){
 				break;
 			}
 		}
-		if(i != ROW_LIST_LENGTH){
-			puts("Game end.");
-			printf("WINNER is %s !\n", colorString[currentColor]);
-			break;
-		}
+		printf("BlackWins: %4d/%4d    (%s)\n", env.playerBlackWins, env.gameCount + 1, playerTypeString[env.playerBlackType]);
+		printf("WhiteWins: %4d/%4d    (%s)\n", env.playerWhiteWins, env.gameCount + 1, playerTypeString[env.playerWhiteType]);
+		printf("     Draw: %4d/%4d\n", env.gameCount - env.playerBlackWins - env.playerWhiteWins + 1, env.gameCount + 1);
+		initBoard(env.mainBoard);
+		env.rowList[0].length = 0;
 	}
 
 	return 0;
@@ -71,11 +91,24 @@ void inputStoneLocation(StoneLocation *loc)
 
 void selectGameMode(GameEnvironment *env)
 {	
+	int i;
+	
+	env->playerBlackWins = 0;
+	env->playerWhiteWins = 0;
 	puts("================================================================");
 	puts("This is a little strong Gomoku program made by hikalium.");
 	puts("================================================================");
-	env->playerBlackType = scanIntegerRanged(0, 1, "Select black player type (0: COM(rand), 1: human): ");
-	env->playerWhiteType = scanIntegerRanged(0, 1, "Select white player type (0: COM(rand), 1: human): ");
+	puts("Player type list.");
+	for(i = 0; i < AI_LIST_LENGTH + 1; i++){
+		printf("%d: %s\n", i, playerTypeString[i]);
+	}
+	env->playerBlackType = scanIntegerRanged(0, AI_LIST_LENGTH, "Select black player type: ");
+	env->playerWhiteType = scanIntegerRanged(0, AI_LIST_LENGTH, "Select white player type: ");
+	if(env->playerBlackType != PLAYER_TYPE_HUMAN && env->playerWhiteType != PLAYER_TYPE_HUMAN){
+		env->gameCountLimit = scanIntegerRanged(1, 65535, "Input number of games (1-65535): ");
+	} else{
+		env->gameCountLimit = 1;
+	}
 	return;
 }
 
@@ -144,7 +177,7 @@ int putStone(char *board, int x, int y, int state)
 void checkMapRows(char *board, StoneRow *rowList, int rowListLen)
 {
 	char checkMap[BOARD_SIZE*BOARD_SIZE];
-	int x, y, count, col, d, rowIndex;
+	int x, y, count, col, d, rowIndex, endType;
 
 	// initialize.
 	for(y = 0; y < BOARD_SIZE; y++){
@@ -152,9 +185,7 @@ void checkMapRows(char *board, StoneRow *rowList, int rowListLen)
 			checkMap[y * BOARD_SIZE + x] = 0;
 		}
 	}
-	for(rowIndex = 0; rowIndex < rowListLen; rowIndex++){
-		rowList[rowIndex].length = 0;
-	}
+	rowList[0].length = 0;
 	rowIndex = 0;
 	for(col = 1; col <= 2; col++){
 		for(d = 1; d <= 8; d <<= 1){
@@ -169,21 +200,25 @@ void checkMapRows(char *board, StoneRow *rowList, int rowListLen)
 					count = 0;
 					checkMapRowsSub(board, checkMap, x, y, d, col, &count);
 					if(count > 1){
-						printf("%s row (%d, %d) %d %d\n", colorString[col], x, y, d, count);
-					}
-					rowList[rowIndex].start.x = x;
-					rowList[rowIndex].start.y = y; 
-					rowList[rowIndex].direction = d;
-					rowList[rowIndex].length = count;
-					rowIndex++;
-					if(rowIndex >= rowListLen){
-						puts("RowList overflow.");
-						return;
+						endType = checkMapRows_getEndType(board, x, y, d, count);
+						printf("%s row %c%d, %d%c %d %d %d\n", colorString[col], ((endType & 1) ? '(' : '['), x, y, (((endType >> 1) & 1) ? ')' : ']'), d, count, endType);
+						rowList[rowIndex].col = col;
+						rowList[rowIndex].start.x = x;
+						rowList[rowIndex].start.y = y; 
+						rowList[rowIndex].direction = d;
+						rowList[rowIndex].length = count;
+						rowList[rowIndex].endType = endType;
+						rowIndex++;
+						if(rowIndex >= rowListLen){
+							puts("RowList overflow.");
+							return;
+						}
 					}
 				}
 			}
 		}
 	}
+	rowList[rowIndex].length = 0;
 }
 
 void checkMapRowsSub(char *board, char *checkMap, int nx, int ny, int d, int col, int *count)
@@ -196,16 +231,94 @@ void checkMapRowsSub(char *board, char *checkMap, int nx, int ny, int d, int col
 	}
 	(*count)++;
 	checkMap[ny * BOARD_SIZE + nx] |= d;
-	if(d == DIRECTION_X){
-		nx++;
-	} else if(d == DIRECTION_Y){
-		ny++;
-	} else if(d == DIRECTION_LDOWN){
-		nx--;
-		ny++;
-	} else if(d == DIRECTION_RDOWN){
-		nx++;
-		ny++;
-	}
+	getLocationOnDirection(&nx, &ny, d, 1);
 	checkMapRowsSub(board, checkMap, nx, ny, d, col, count);
-} 
+}
+
+int checkMapRows_getEndType(char *board, int x, int y, int d, int count)
+{
+	int endType = 0;
+	int px, py;
+	
+	// start
+	px = x;
+	py = y;
+	getLocationOnDirection(&px, &py, d, -1);
+	if(px != -1 && board[py * BOARD_SIZE + px] == STATE_NONE){
+		endType |= ENDTYPE_CAN_PUT;
+	}
+	
+	// end
+	px = x;
+	py = y;
+	getLocationOnDirection(&px, &py, d, count);
+	if(px != -1 && board[py * BOARD_SIZE + px] == STATE_NONE){
+		endType |= (ENDTYPE_CAN_PUT << 1);
+	}
+	return endType;
+}
+
+void getLocationOnDirection(int *x, int *y, int d, int count)
+{
+	// (*x, *y) is set (-1, -1) when location after moving is out of bound.
+	int dx = 0, dy = 0;
+	if(d == DIRECTION_X){
+		dx++;
+	} else if(d == DIRECTION_Y){
+		dy++;
+	} else if(d == DIRECTION_LDOWN){
+		dx--;
+		dy++;
+	} else if(d == DIRECTION_RDOWN){
+		dx++;
+		dy++;
+	}
+	*x += dx * count;
+	*y += dy * count;
+	if(*x < 0 || BOARD_SIZE <= *x || *y < 0 || BOARD_SIZE <= *y){
+		*x = -1;
+		*y = -1;
+	}
+}
+
+int isGameEnd(GameEnvironment *env)
+{
+	int i, x, y;
+
+	checkMapRows(env->mainBoard, env->rowList, ROW_LIST_LENGTH);
+	for(i = 0; i < ROW_LIST_LENGTH; i++){
+		if(env->rowList[i].length == 0){
+			break;
+		}
+		if(env->rowList[i].length == 5){
+			break;
+		}
+	}
+	if(i != ROW_LIST_LENGTH && env->rowList[i].length == 5){
+		puts("Game end.");
+		printf("WINNER is %s !\n", colorString[env->currentColor]);
+		if(env->currentColor == STATE_BLACK){
+			env->playerBlackWins++;
+		} else{
+			env->playerWhiteWins++;
+		}
+		return 1;
+	}
+	for(y = 0; y < BOARD_SIZE; y++){
+		for(x = 0; x < BOARD_SIZE; x++){
+			if(env->mainBoard[y * BOARD_SIZE + x] == STATE_NONE){
+				break;
+			}
+		}
+		if(x != BOARD_SIZE){
+			break;
+		}
+	}
+	if(y == BOARD_SIZE){
+		puts("Game end.");
+		printf("Game ended in draw... \n");
+		return 1;
+	}
+
+	return 0;
+}
--- gomoku.h
+++ gomoku.h
@@ -1,34 +1,20 @@
 #define BOARD_SIZE	15
-#define ROW_LIST_LENGTH (BOARD_SIZE * BOARD_SIZE)
+#define ROW_LIST_LENGTH (BOARD_SIZE * BOARD_SIZE * 4)
 
 #define STATE_NONE	0
 #define STATE_BLACK	1
 #define STATE_WHITE	2
 
-#define PLAYER_TYPE_COM_RANDOM	0
-#define PLAYER_TYPE_HUMAN		1
+#define PLAYER_TYPE_HUMAN		0
+#define PLAYER_TYPE_AI_RAND		1
+#define AI_LIST_LENGTH			2
 
 #define DIRECTION_X		1
 #define DIRECTION_Y		2
 #define DIRECTION_LDOWN	4
 #define DIRECTION_RDOWN 8
 
-char *stateString[] = {
-	".",
-	"●",
-	"○"
-};
-
-char *colorString[] = {
-	"None",
-	"Black",
-	"White"
-};
-
-char *playerTypeString[] = {
-	"COM(rand)",
-	"Human"
-};
+#define ENDTYPE_CAN_PUT		1
 
 typedef struct STONE_LOCATION StoneLocation;
 struct STONE_LOCATION {
@@ -37,20 +23,35 @@ struct STONE_LOCATION {
 
 typedef struct STONE_ROW StoneRow;
 struct STONE_ROW {
+	int col;
 	StoneLocation start;
 	int direction;
 	int length; // > 1.
+	int endType;
+	// bit 0: start
+	// bit 1: end
 };
 
 typedef struct GAME_ENVIRONMENT GameEnvironment;
 struct GAME_ENVIRONMENT {
 	int playerBlackType;
 	int playerWhiteType;
+	int playerBlackWins;
+	int playerWhiteWins;
 	char mainBoard[BOARD_SIZE*BOARD_SIZE];
 	StoneRow rowList[ROW_LIST_LENGTH];
 	int turnCount;	// even: black, odd: white
+	int gameCount;
+	int gameCountLimit;
+	int currentColor;
+	int currentPlayerType;
 };
 
+extern char *stateString[];
+extern char *colorString[];
+extern char *playerTypeString[];
+
+// @gomoku.c
 void inputStoneLocation(StoneLocation *loc);
 void selectGameMode(GameEnvironment *env);
 int scanIntegerRanged(int from, int to, char *message);
@@ -59,3 +60,10 @@ void printBoard(char *board);
 int putStone(char *board, int x, int y, int state);
 void checkMapRows(char *board, StoneRow *rowList, int rowListLen);
 void checkMapRowsSub(char *board, char *checkMap, int nx, int ny, int d, int col, int *count);
+int checkMapRows_getEndType(char *board, int x, int y, int d, int count);
+void getLocationOnDirection(int *x, int *y, int d, int count);
+int isGameEnd(GameEnvironment *env);
+
+// @gomokuai.c
+void RandAI_decideNextLocation(StoneLocation *to, GameEnvironment *env);
+void EasyAI_decideNextLocation(StoneLocation *to, GameEnvironment *env);
